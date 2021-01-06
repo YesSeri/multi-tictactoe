1924c26b58362d5aca2d190bd3e8367073a73987
yesseri <henrik.zenkert@gmail.com>
2021-01-06 11:34:25 +0100

    made a join create server function in server.js

--- multi-tictactoe/server.js
+++ multi-tictactoe/server.js
@@ -7,32 +7,42 @@ const io = require('socket.io')(httpServer, options);
 
 const PORT = process.env.PORT | 5000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World');
-});
-
-const roomName = 'testRoom';
-const roomNumber = 1;
-const message = 'this is a test message'
-const object = { description: 'this is a test object' }
+//app.get('/', (req, res) => {
+//	res.send('Hello World');
+//});
 
 class Room {
   constructor(name, clientX, clientO) {
-		this.name=name
-		this.clientX=clientX
-		this.clientO=clientO
+      this.name = name;
+      this.clientX = clientX;
+      this.clientO = clientO;
   }
 }
 
-let clients = 0;
+let rooms = [];
+
 io.on('connection', (socket) => {
-	socket.on('join', (room) => {
-		clients++;
-		console.log('number of clients: ', clients)
+	socket.on('join', (room) => { // Will have three fields. One is room.name. The other is room.join and is a boolean. If true join room else create room. Last one is isX. Is only used when creating room, for making player able to choose char.
+    if (room.join){ // If room exists we join that room. 
+      console.log('Joining room', room.name) //Need to find room and add user to it.
+    } else { // If not we create a new room that someone else can join
+      console.log('Creating room', room.name)
+      const tempRoom = new Room(room.name, room.isX, !room.isX)
+    }
+    
 		socket.join(room);
 		console.log('room: ', room)
   });
 });
+
+function roomExists(newRoomName) { // Rooms is global
+  for (let room of rooms) {
+    if (room.name === newRoomName) {
+      return false;
+    }
+  }
+  return true;
+}
 
 httpServer.listen(PORT, () => {
 	console.log(`Listening on port: ${PORT}`);
--- roomClassTest.js
+++ roomClassTest.js
@@ -1,14 +1,18 @@
 class Room {
-    constructor(name, clientX, clientO) {
-        this.name = name;
-        this.clientX = clientX;
-        this.clientO = clientO;
-    }
-    function toString() {
-    }
+  constructor(name, clientX, clientO) {
+      this.name = name;
+      this.clientX = clientX;
+      this.clientO = clientO;
+  }
 }
-
 const room = new Room('aaa333', 'pelle', 'kalle')
 
-console.log(room.toString())
-console.log(Object.getOwnPropertyNames(String.prototype))
+const a = {
+  aa33: {
+    clientX: 'kalle',
+    clientO: 'pelle'
+  }
+}
+console.log(a);
+
+//console.log(Object.getOwnPropertyNames(String.prototype))
